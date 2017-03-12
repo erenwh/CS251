b4df05a039e9e989e704319f441861e459b7713e
HanWang <erenwh@gmail.com>
2017-03-12 04:50:19 -0500

    organize folder

---
+++ projects/cmake-build-debug/CMakeFiles/CS251.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS251.dir/project3/brute.cpp.o"
+  "CMakeFiles/CS251.dir/project3/encrypt.cpp.o"
+  "CMakeFiles/CS251.dir/project3/symbol.cpp.o"
+  "CS251.pdb"
+  "CS251"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS251.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
